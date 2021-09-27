ae0d7c5f3dfc9b86b334d7300a46cf0775849f35
Shin Oishi <soishi123@gmail.com>
2021-09-27 15:56:33 -0700

    Completed prep-javascript-arrays exercise.

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,21 @@
+var language = [
+  'html',
+  'css',
+  'javascript',
+  'react',
+  'node',
+  'postgres'
+];
+console.log('languages array', language);
+
+var firstElement = 'html';
+console.log('the first element of the array is', firstElement);
+
+var thirdElement = 'javascript';
+console.log('the 3rd element of the array is', thirdElement);
+
+var length = '6';
+console.log('The length of the array is', length);
+
+var lastElement = language[language.length - 1];
+console.log('The last element of the array is', lastElement);
